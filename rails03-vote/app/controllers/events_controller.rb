583b6cec053c122be71242f9826ed54e97938218
ITO Yosei <y-itou@lumber-mill.co.jp>
2025-05-03 06:44:36 +0900

    fix: Added missing parameter.

--- rails03-vote/app/controllers/events_controller.rb
+++ rails03-vote/app/controllers/events_controller.rb
@@ -78,6 +78,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.expect(event: [ :title, :schedules, :user_id ])
+      params.expect(event: [ :title, :schedules, :user_id, :description ])
     end
 end
